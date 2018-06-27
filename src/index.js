93cad660681d2f5730457a9247b362fa13233a26
michellecarey <genovese28@yahoo.com>
2018-06-27 00:16:50 -0700

    adding redux

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
+import { searchRobots } from './reducers';
 import tachyons from 'tachyons';
 //import { robots } from './robots';
 
+const store = createStore(searchRobots);
+
 ReactDOM.render(
-  //everything has to be wrapped in a div and you can only retuen one
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
 registerServiceWorker();
